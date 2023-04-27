40940a3a0ee255855847e26a4010d3d5d1e0c581
Ali <lariali1986@gmail.com>
2023-04-27 16:37:45 -0400

    no technical change

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from './components/HomeScreen';
-import LoginScreen from './components/LoginScreen';
+import HomeScreen from "./components/HomeScreen";
+import LoginScreen from "./components/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
+        <Stack.Screen
           name="Home"
-          component={ HomeScreen }
-          // options={{title: 'Welcome' }} 
-          />
-      <Stack.Screen name="Login" component={LoginScreen} />
+          component={HomeScreen}
+          // options={{title: 'Welcome' }}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
-    </NavigationContainer>               
+    </NavigationContainer>
   );
 }
-
