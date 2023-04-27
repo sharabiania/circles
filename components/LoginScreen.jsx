b83c44df31de760b27da0927ee1a3e15c1b8be0c
Ali <lariali1986@gmail.com>
2023-04-27 16:15:52 -0400

    1-storing the toke ,2- login disabled when no user and pass provided, 3-navigate to home  screen when credentials correct 4-Overlays

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,29 +1,63 @@
-import React from 'react'
-import { Button, TextInput, Text, SafeAreaView, StyleSheet } from 'react-native';
-import Register from './Register';
+import React, { useState } from "react";
+import {
+  Button,
+  TextInput,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
+import Register from "./Register";
+import { logInUser } from "../util/auth";
+//import Snackbar from "react-native-snackbar";
+import AuthContextProvider, { AuthContext } from "../store/auth-context";
+import LoadingOverlay from "./ui/LaodingOverlays";
 
 // NOTE: use this maybe: https://www.npmjs.com/package/react-native-login-screen
-export default function LoginScreen({navigation}) {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+export default function LoginScreen({ navigation }) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-    return (
-        <SafeAreaView>
-          <Text>Login</Text>
-          <TextInput 
-            placeholder='Username'
-            style={styles.input}
-            onChangeText={setUsername} value={username} />
-          <TextInput 
-            placeholder='Password'
-            style={styles.input}
-            secureTextEntry={true}
-            onChangeText={setPassword} value={password} />
-          <Button title='login' />
-          <Register/> 
-                   
-        </SafeAreaView>
-    );
+  async function logInHandler() {
+    setIsAuthenticating(true);
+    try {
+      const token = await logInUser(username, password);
+      console.log(token);
+      navigation.navigate("Home");
+    } catch (error) {
+      alert("Your credentials is not correct", "Try again");
+      setIsAuthenticating(false);
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Username"
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Button
+        title="login"
+        disabled={!(password && username)}
+        onPress={logInHandler}
+      />
+      <Register />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -31,6 +65,6 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 12,
     borderWidth: 1,
-    padding: 10
-  }
+    padding: 10,
+  },
 });
