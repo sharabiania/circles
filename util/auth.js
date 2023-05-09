0ffe0db841bfa17cd70f6f4c4816d6c107ac6bc9
Ali <lariali1986@gmail.com>
2023-05-09 18:17:16 -0400

    add post Rq for signUP

--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,20 @@
-const url = "https://d94o460neg.execute-api.us-east-1.amazonaws.com/api/login";
+const url = "https://d94o460neg.execute-api.us-east-1.amazonaws.com";
+
 export async function login(username, password) {
-  const response = await fetch(url, {
+  const response = await fetch(url+"/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
+  });
+  //const data = await response.text();
+  return response;
+}
+
+export async function signup(username, password,email) {
+  const response = await fetch(url+"/api/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password, email}),
   });
   //const data = await response.text();
   return response;
