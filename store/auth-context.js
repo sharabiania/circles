2a68963ddd9fc3a86264ca124998dde705321410
lariali1986 <ali.lari@mail.concordia.ca>
2023-05-18 14:35:02 -0400

    adding some variales to extract user email and expiration

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,23 +1,50 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
 export const AuthContent = createContext({});
 
 export default function AuthContentProvider(props) {
-   const [username, setUsername]=useState('');
-   const [token, setToken] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [email, setEmail] = useState('');
+  const [expiration, setExpiration] = useState('');
 
-   const setAuthUsername=(username)=>{
-        setUsername(username)
-    }
-    const setAuthToken=(token)=>{
-      setToken(token)
-    }
-  const value = {
-    storedUsername: username,
+  const setAuthUsername = (username) => {
+    setUsername(username);
+  };
+  const setAuthPass = (username) => {
+    setPassword(username);
+  };
+  const setAuthToken = (token) => {
+    setToken(token);
+  };
+  const setEmailAdress = (email) => {
+    setEmail(email);
+  };
+  const setExpTime = (expiration) => {
+    setExpiration(expiration);
+  };
+
+  const storedInfo = {
+    username: username,
+    password: password,
+    email: email,
     token: token,
     isAuthenticated: !!token,
+    expiration: expiration,
+  };
+
+  const setFcn = {
     setAuthToken: setAuthToken,
     setAuthUsername: setAuthUsername,
+    setEmailAdress: setEmailAdress,
+    setExpTime: setExpTime,
+    setAuthPass: setAuthPass,
+  };
+
+  const value = {
+    storedInfo: storedInfo,
+    setFcn: setFcn,
   };
 
   return (
