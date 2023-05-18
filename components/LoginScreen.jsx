b5832aded7b4f14aeebb4255d894ce91b5035f5c
lariali1986 <ali.lari@mail.concordia.ca>
2023-05-18 14:12:14 -0400

    register component removed to be separated from login screen

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,81 +1,84 @@
-import { useContext, useState } from "react";
+import { useContext, useState } from 'react';
 import {
   Text,
   SafeAreaView,
   TextInput,
   StyleSheet,
   Button,
-} from "react-native";
-import { AuthContent } from "../store/auth-context";
-import { login } from "../util/auth";
-import Register from "./Register";
-import LoadingOverlay from "./ui/LoadingOverlays";
-import jwt_decode from "jwt-decode";
+} from 'react-native';
+import { AuthContent } from '../store/auth-context';
+import { login } from '../util/auth';
+import LoadingOverlay from './ui/LoadingOverlays';
+import jwt_decode from 'jwt-decode';
 
 export default function LoginScreen({ navigation }) {
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  const { setAuthUsername, setAuthToken, storedUsername } =
-    useContext(AuthContent);
+  const { setFcn } = useContext(AuthContent);
 
-  async function checkValidAuth(response, status){
-    if (status == 401) {
-      alert("credentials not correct");
-    } else if (status == 200) {
-      const token = await response.text();
-      const decoded = jwt_decode(token);
-      const receivedUsername = decoded["cognito:username"];
-      if (username == receivedUsername) {
-        setAuthUsername(receivedUsername);
-        setAuthToken(decoded.event_id);
-        navigation.navigate("Home");
-      }
+  function sparseJWT(token) {
+    setFcn.setAuthToken(token);
+    let decodedJWT = jwt_decode(token);
+    if (username == decodedJWT['cognito:username']) {
+      setFcn.setAuthUsername(username);
+      setFcn.setEmailAdress(decodedJWT['email']);
+      setFcn.setExpTime(decodedJWT['exp']);
+      navigation.navigate('Home');
+    } else {
+      setFcn.setAuthToken('null');
     }
   }
-  
+
+  async function checkValidAuth(response, status) {
+    if (status == 401) {
+      alert('credentials not correct');
+    } else if (status == 200) {
+      const token = await response.text();
+      sparseJWT(token);
+    }
+  }
+
   async function authHandler() {
     setIsAuthenticating(true);
     try {
       const response = await login(username, password);
       const status = response.status;
-      checkValidAuth(response, status)
+      checkValidAuth(response, status);
       setIsAuthenticating(false);
     } catch (error) {
-      alert("server is down");
+      alert('server is down');
       setAuthToken(null);
       setIsAuthenticating(false);
     }
   }
   if (isAuthenticating) {
-    return <LoadingOverlay message="Logging you in..." />;
+    return <LoadingOverlay message='Logging you in...' />;
   }
 
   return (
     <SafeAreaView>
       <Text>Login</Text>
       <TextInput
-        placeholder="Username"
+        placeholder='Username'
         style={styles.input}
         onChangeText={setUsername}
         value={username}
       />
 
       <TextInput
-        placeholder="Password"
+        placeholder='Password'
         style={styles.input}
         secureTextEntry={true}
         onChangeText={setPassword}
         value={password}
       />
       <Button
-        title="login"
+        title='login'
         disabled={!(password && username)}
         onPress={authHandler}
       />
-      <Register />
     </SafeAreaView>
   );
 }
@@ -84,7 +87,7 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderRadius: 6,
     borderWidth: 1,
     padding: 10,
