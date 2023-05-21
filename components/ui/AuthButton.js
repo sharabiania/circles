9138999245634f4809b4ab0a8b79df3255b73a40
lariali1986 <ali.lari@mail.concordia.ca>
2023-05-20 20:11:39 -0400

    SignUp button changed oto Auth Button

---
+++ components/ui/AuthButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const AuthButton = ({ title, onPress }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+export default AuthButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'white',
+    padding: 2,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  buttonText: {
+    color: 'gray',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
+
